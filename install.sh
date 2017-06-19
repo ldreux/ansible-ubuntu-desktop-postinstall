0e634ebabaeab8b9f0bd8a5d5e775f4b41c2e7d6
Loïc Dreux <loic.dreux@sarbacane.com>
2017-06-19 10:21:18 +0200

    Add maven

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
-git clone
+ansible-playbook -i localhost.inventory desktop-install.yml  -K
